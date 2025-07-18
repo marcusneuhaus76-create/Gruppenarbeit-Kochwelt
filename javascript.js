9aec581a157b7eab0ff83bcd77ca9e9fa3150e0d
Renato Lüpnitz <renato13@gmx.net>
2025-07-18 18:23:10 +0200

    reponsiv+mobil menu

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById('burger');
+const nav = document.getElementById('header_menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  nav.classList.toggle('show');
+});
